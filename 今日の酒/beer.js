c5351e01591c36074923c51d65d10d3729a7d315
yusei66 <gai.face2001@icloud.com>
2021-03-06 02:25:22 +0900

    お酒移動の仕組み制作、関西ならず

--- 今日の酒/beer.js
+++ 今日の酒/beer.js
@@ -1,39 +1,5 @@
-function onClick () {
-    const texts = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
-    const images = ["osake1.jpeg", "osake2.jpeg", "osake3.jpeg", "osake4.jpeg", "osake5.jpeg", "osake6.jpeg", "osake7.jpeg", "osake8.jpeg", "osake9.jpeg", "osake10.jpeg", "osake11.jpeg", "osake12.jpeg", "osake13.jpeg", "osake14.jpeg", "osake15.jpeg", "osake16.jpeg", "osake17.jpeg", "osake18.jpeg", "osake19.jpeg", "osake20.jpeg"]
-    setImage("umesyu", texts, images)
-}
+const imageArea = document.getElementById('imageArea');
+const images = ['#link1', '#link2'];
 
-function setImage (id, texts, images) {
-    const resultInt = Math.floor(Math.random() * images.length);
-    const resultText = texts[resultInt]
-    const resultImg = images[resultInt]
-    document.getElementById(id).src = "sake-sozai/" + resultImg;
-    document.getElementById(id).alt = resultText;
-}
-function atClick () {
-    const texts = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
-    const images = ["osake1.jpeg", "osake2.jpeg", "osake3.jpeg", "osake4.jpeg", "osake5.jpeg", "osake6.jpeg", "osake7.jpeg", "osake8.jpeg", "osake9.jpeg", "osake10.jpeg", "osake11.jpeg", "osake12.jpeg", "osake13.jpeg", "osake14.jpeg", "osake15.jpeg", "osake16.jpeg", "osake17.jpeg", "osake18.jpeg", "osake19.jpeg", "osake20.jpeg"]
-    setImage("umesyu", texts, images)
-}
-
-function setImage (id, texts, images) {
-    const resultInt = Math.floor(Math.random() * images.length);
-    const resultText = texts[resultInt]
-    const resultImg = images[resultInt]
-    document.getElementById(id).src = "sake-sozai/" + resultImg;
-    document.getElementById(id).alt = resultText;
-}
-function theClick () {
-    const texts = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
-    const images = ["osake1.jpeg", "osake2.jpeg", "osake3.jpeg", "osake4.jpeg", "osake5.jpeg", "osake6.jpeg", "osake7.jpeg", "osake8.jpeg", "osake9.jpeg", "osake10.jpeg", "osake11.jpeg", "osake12.jpeg", "osake13.jpeg", "osake14.jpeg", "osake15.jpeg", "osake16.jpeg", "osake17.jpeg", "osake18.jpeg", "osake19.jpeg", "osake20.jpeg"]
-    setImage("umesyu", texts, images)
-}
-
-function setImage (id, texts, images) {
-    const resultInt = Math.floor(Math.random() * images.length);
-    const resultText = texts[resultInt]
-    const resultImg = images[resultInt]
-    document.getElementById(id).src = "sake-sozai/" + resultImg;
-    document.getElementById(id).alt = resultText;
-}
+const imageNo = Math.floor( Math.random() * images.length)
+imageArea.src = images[imageNo];
