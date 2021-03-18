0b849e003d6940f06adbeaa168c2b9333215e27f
yusei66 <gai.face2001@icloud.com>
2021-03-19 00:45:57 +0900

    酒などを戻す

--- kitikiti.js
+++ kitikiti.js
@@ -1,9 +1,7 @@
 function onClick() {
-    var resultInt = Math.floor(Math.random() * 8)
-    var resultText = ["大凶","凶","平","吉","小吉","末吉","中吉","大吉"][resultInt]
-    var resultImg = ["daikyou.jpg","kyou.jpg","hei.jpg","kichi.jpg","syoukichi.jpg","suekichi.jpg","tyuukichi.jpg","daikichi.jpg"][resultInt]
-    document.getElementById("omikuji").src = "images-omikuji/" + resultImg;
+    var resultInt = Math.floor(Math.random() * 5)
+    var resultText = ["吉","笑吉","末吉","中吉","大吉"][resultInt]
+    var resultImg = ["omikuji-1.png","omikuji-2.png","omikuji-3.png","omikuji-4.png","omikuji-5.png"][resultInt]
+    document.getElementById("omikuji").src = resultImg;
     document.getElementById("omikuji").alt = resultText;
 }
-
-setTimeout(function(){location.href="http://localhost:63342/big-pad-1/index.html?_ijt=igp3q3pbr97rnb7mf87drr0k95"} , 8000);
